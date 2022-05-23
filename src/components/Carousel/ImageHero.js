e964b691276d6ada1df730754753cf342bc15ec0
Kaylee Stevens <kayls@Kaylees-MBP.lan>
2022-05-23 12:15:16 -0500

    auto scroll and top carousel

--- src/components/Carousel/ImageHero.js
+++ src/components/Carousel/ImageHero.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
-import { SliderData } from '../Slider-2/SliderData';
-import { HeroData } from './HeroData'; 
+import React, { useState, useEffect } from 'react';
+import { HeroData } from './HeroData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
-import { TextData } from './TextData';
 import './slider.css';
+
+
 
 const ImageHero = ({ slides }) => {
     const [current, setCurrent] = useState(0)
     const length = slides.length;
 
+    const autoScroll = true;
+    let slideInterval;
+    let intervalTime = 5000;
+
+    function auto() {
+        slideInterval = setInterval(nextSlide, intervalTime)
+    }
+
+    useEffect(() => {
+        if (autoScroll) {
+            auto()
+        } 
+        return () => clearInterval(slideInterval)
+    }, [current])
+
     const nextSlide = () => {
-        setCurrent(current === length -1 ? 0 : current + 1);
+        setCurrent(current === length - 1 ? 0 : current + 1);
     };
-    
-      const prevSlide = () => {
-          setCurrent(current === 0 ? length -1 : current -1);
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
     };
-    if(!Array.isArray(slides) || slides.length <= 0) {
+    if (!Array.isArray(slides) || slides.length <= 0) {
         return null;
     }
 
@@ -24,36 +39,41 @@ const ImageHero = ({ slides }) => {
         <section>
             <div className='text-hero'>
                 <h1>The World's Most</h1>
-                {/* {TextData.map((text, index) => {
+                {HeroData.map((header, index) => {
                     return (
                         <div className={index === current ? 'text active' : 'text'} key={index}>
-                        {index === current && (<p text={text.url} className='inside-text'/>)}
+                        {index === current && (
+                            <div className='top-content'>
+                                <h2>{header.header}</h2>
+                            </div>
+                        )}
                     </div>
                 )},
                 )
-                    }) */}
+                    }
                 <h1>Electronic Stethoscope</h1>
-                
+
             </div>
-        <div className='hero-icons'>
-            <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-            <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-            {HeroData.map((pictures, index) => {
-                return(
-                <div className={index === current ? 'slide active' : 'slide'} key={index}>
-                {index === current && (
-                <div>
-                <img src={pictures.image} alt="Hero IMG" className='hero-image' />
-                <div className='content'>
-                    <h2>{pictures.heading}</h2>
-                    <p>{pictures.desc}</p>
-                </div>
-                </div>
-                )}
-                </div>        
-                )
-            })}
-        </div>
+            <div className='hero-icons'>
+                <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+                <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+                {HeroData.map((pictures, index) => {
+                    return (
+                        <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                            {index === current && (
+                                <div>
+                                    <img src={pictures.image} alt="Hero IMG" className='hero-image' />
+                                    <div className='content'>
+                                        <h2>{pictures.heading}</h2>
+                                        <hr></hr>
+                                        <p>{pictures.desc}</p>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    )
+                })}
+            </div>
         </section>
     )
 };
