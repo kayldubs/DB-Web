7b627a69d532179598a9736ae369c8be60959fc3
Kaylee Stevens <kstevensgraphicarts@gmail.com>
2022-06-21 13:06:42 -0500

    added new device image

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -17,6 +17,7 @@ import Telemedical from '../../assests/imgs/teleVector.svg';
 import Record from '../../assests/imgs/recordVector.svg';
 import Demand from '../../assests/imgs/demandVector.svg';
 import IMG from '../../assests/imgs/infoCard-04.svg';
+import IMG2 from '../../assests/imgs/deviceInfo.svg';
 
 //import { borderRadius, fontWeight } from '@material-ui/system';
 
@@ -37,8 +38,11 @@ function Features() {
             />
           </div>
           <div className='media2' class='col-lg-6'>
-            <h1>Test Header</h1>
-            <p>text content</p>
+          <CardMedia
+              component="img"
+              alt="heart with connections to DigiBeat Logo"
+              image={IMG2}
+            />
           </div>
         </Card>
         <br></br>
