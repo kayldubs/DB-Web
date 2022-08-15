7b90cf9232a2c7b992a2ab16846652eb8a77aced
Kaylee Stevens <kstevensgraphicarts@gmail.com>
2022-08-15 11:43:03 -0500

    edit server.js, not using app.js anymore

--- server/server.js
+++ server/server.js
@@ -1,67 +1,49 @@
-// const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
-// const path = require('path');
-// require("dotenv").config();
-
-// const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
-// const db = require('./config/connection');
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// const startServer = async () => {
-//   const server = new ApolloServer({ 
-//     // typeDefs, 
-//     // resolvers, 
-//     context: authMiddleware 
-//   });
-
-//   await server.start();
-
-//   server.applyMiddleware({ app });
-
-//   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-// };
-
-// startServer();
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// // Serve up static assets
-// app.use('/imgs', express.static(path.join(__dirname, '../../src/assests/imgs')));
-
-// // if (process.env.NODE_ENV === 'production') {
-// //   app.use(express.static(path.join(__dirname, '../src/build')));
-// // }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
-
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//   });
-// })
-
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+require("dotenv").config();
 
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const path = require('path');
+const startServer = async () => {
+  const server = new ApolloServer({ 
+    // typeDefs, 
+    // resolvers, 
+    context: authMiddleware 
+  });
 
-const port = process.env.PORT || 5000;
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+app.use('/imgs', express.static(path.join(__dirname, '../../src/assests/imgs')));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
+  app.use(express.static(path.join(__dirname, '../src/build')));
 }
 
-app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log('Server running on port: ', port);
+app.get('*', (req, res) => {
+  req.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 })
+
+
