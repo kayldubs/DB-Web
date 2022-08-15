4756073d867ee019a506ac2ae63b197801154e39
Kaylee Stevens <kstevensgraphicarts@gmail.com>
2022-08-15 11:11:21 -0500

    changed app.js to server UI website

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        req.sendFile(path.join(publicPath, 'index.html'));
     })
-}
+// }
 
 app.listen(port, (err) => {
     if (err) return console.log(err);
-    console.log('Server running on port: ', port);
-})
+    console.log(`Server is up on ${port}`);
+});
--- server/server.js
+++ server/server.js
@@ -1,47 +1,47 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-require("dotenv").config();
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
+// require("dotenv").config();
 
 // const { typeDefs, resolvers } = require('./schemas');
 // const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-const startServer = async () => {
-  const server = new ApolloServer({ 
-    // typeDefs, 
-    // resolvers, 
-    context: authMiddleware 
-  });
+// const startServer = async () => {
+//   const server = new ApolloServer({ 
+//     // typeDefs, 
+//     // resolvers, 
+//     context: authMiddleware 
+//   });
 
-  await server.start();
+//   await server.start();
 
-  server.applyMiddleware({ app });
+//   server.applyMiddleware({ app });
 
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+//   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+// };
 
-startServer();
+// startServer();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// Serve up static assets
-app.use('/imgs', express.static(path.join(__dirname, '../../src/assests/imgs')));
+// // Serve up static assets
+// app.use('/imgs', express.static(path.join(__dirname, '../../src/assests/imgs')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../src/build')));
-// }
+// // if (process.env.NODE_ENV === 'production') {
+// //   app.use(express.static(path.join(__dirname, '../src/build')));
+// // }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-})
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//   });
+// })
