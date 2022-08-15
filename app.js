410fe380ad4695db4ecf0b7d64f222faefa6b9b6
Kaylee Stevens <kstevensgraphicarts@gmail.com>
2022-08-15 11:25:56 -0500

    new app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '..', 'build');
 
@@ -13,7 +13,7 @@ app.use(express.static(publicPath));
 // if (process.env.NODE_ENV === 'production') {
 //     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        req.sendFile(path.join(publicPath, 'index.html'));
+        res.sendFile(path.join(publicPath, 'index.html'));
     })
 // }
 
